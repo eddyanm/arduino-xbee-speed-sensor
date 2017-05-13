ec3a98153bec075732279f7b6d08719318eace8b
eddyanm <eddyanm@gmail.com>
2017-05-12 22:40:39 -0700

    Add files via upload

--- signal-generator/signal-generator.ino
+++ signal-generator/signal-generator.ino
@@ -13,16 +13,15 @@
 */
 
 int RXLED = 17;  // The RX LED has a defined Arduino pin
-// The TX LED was not so lucky, we'll need to use pre-defined
-// macros (TXLED1, TXLED0) to control that.
-// (We could use the same macros for the RX LED too -- RXLED1,
-//  and RXLED0.)
-
-//int inByte = 0;
 
 unsigned long time = 0;
 unsigned long lastTime = 0;
-unsigned outputPeriod = 300;
+unsigned long lastChangeTime = 0;
+unsigned long changePeriod = 500000; // 0.5 sec
+float outputFreqHigh = 150;  // 500 Hz
+float outputFreqLow = 0.4; // 0.4 Hz
+float outputChangeRate = 0.1;  // 0.1 Hz
+float outputFreq = outputFreqLow;
 
 int outputPin = 7;
 boolean outputState = 0;
@@ -31,36 +30,23 @@ void setup()
 {
   pinMode(outputPin, OUTPUT);
   pinMode(RXLED, OUTPUT);  // Set RX LED as an output
- // TX LED is set as an output behind the scenes
-
- //Serial.begin(115200); //This pipes to the serial monitor
- //Serial1.begin(115200); //This is the UART, pipes to sensors attached to board
 }
 
 void loop()
 {
-  time = millis();
+  time = micros();
 
-  if (time-lastTime >= outputPeriod) {
+  if (time-lastChangeTime >= changePeriod) {
+    if (outputFreq >= outputFreqHigh)
+      outputFreq = outputFreqLow;
+    outputFreq = outputFreq + outputChangeRate;
+    lastChangeTime = time;
+  }
+  if (time-lastTime >= 1000000/outputFreq) {
     outputState = !outputState;
     digitalWrite(outputPin, outputState);
     digitalWrite(RXLED, outputState);
     lastTime = time;
   }
- /* Serial.println("Hello world");  // Print "Hello World" to the Serial Monitor
- Serial1.println("Hello!");  // Print "Hello!" over hardware UART
-
- while(Serial1.available() > 0) {
-  inByte = Serial1.read();
-  Serial.write(inByte);
- }
- Serial.write('\n');
- digitalWrite(RXLED, LOW);   // set the LED on
- TXLED0; //TX LED is not tied to a normally controlled pin
- delay(1000);              // wait for a second
- digitalWrite(RXLED, HIGH);    // set the LED off
- TXLED1;
- delay(1000);              // wait for a second
-  */
 }
 
